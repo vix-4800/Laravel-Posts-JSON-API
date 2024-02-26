306b73f35ed9e711adab54f3c829bf774b06bac0
vix-4800 <Gordan.ei@protonmail.ch>
2024-02-26 16:16:21 +0400

    converting exceptions to the JSON:API format

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,14 +3,25 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use LaravelJsonApi\Core\Exceptions\JsonApiException;
+use LaravelJsonApi\Exceptions\ExceptionParser;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
+     * A list of the exception types that are not reported.
      *
-     * @var array<int, string>
+     * @var array
+     */
+    protected $dontReport = [
+        JsonApiException::class,
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
      */
     protected $dontFlash = [
         'current_password',
@@ -20,11 +31,17 @@ class Handler extends ExceptionHandler
 
     /**
      * Register the exception handling callbacks for the application.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+
+        $this->renderable(
+            ExceptionParser::make()->renderable()
+        );
     }
 }
