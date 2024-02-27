7392e665543b896039f01c59d373524b321ba675
vix-4800 <Gordan.ei@protonmail.ch>
2024-02-27 12:41:18 +0400

    Category model, migration, policy and schema added

---
+++ app/JsonApi/V1/Categories/CategorySchema.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\JsonApi\V1\Categories;
+
+use App\Models\Category;
+use LaravelJsonApi\Eloquent\Contracts\Paginator;
+use LaravelJsonApi\Eloquent\Fields\DateTime;
+use LaravelJsonApi\Eloquent\Fields\ID;
+use LaravelJsonApi\Eloquent\Filters\WhereIdIn;
+use LaravelJsonApi\Eloquent\Pagination\PagePagination;
+use LaravelJsonApi\Eloquent\Schema;
+
+class CategorySchema extends Schema
+{
+    /**
+     * The model the schema corresponds to.
+     */
+    public static string $model = Category::class;
+
+    /**
+     * Get the resource fields.
+     */
+    public function fields(): array
+    {
+        return [
+            ID::make(),
+            DateTime::make('createdAt')->sortable()->readOnly(),
+            DateTime::make('updatedAt')->sortable()->readOnly(),
+        ];
+    }
+
+    /**
+     * Get the resource filters.
+     */
+    public function filters(): array
+    {
+        return [
+            WhereIdIn::make($this),
+        ];
+    }
+
+    /**
+     * Get the resource paginator.
+     */
+    public function pagination(): ?Paginator
+    {
+        return PagePagination::make();
+    }
+}
