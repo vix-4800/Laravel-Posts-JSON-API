8ea001326dd721475c009880f4acb65bbba34baa
vix-4800 <Gordan.ei@protonmail.ch>
2024-02-27 12:41:35 +0400

    posts query validation added

---
+++ app/JsonApi/V1/Posts/PostCollectionQuery.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\JsonApi\V1\Posts;
+
+use LaravelJsonApi\Laravel\Http\Requests\ResourceQuery;
+use LaravelJsonApi\Validation\Rule as JsonApiRule;
+
+class PostCollectionQuery extends ResourceQuery
+{
+    /**
+     * Get the validation rules that apply to the request query parameters.
+     */
+    public function rules(): array
+    {
+        return [
+            'fields' => [
+                'nullable',
+                'array',
+                JsonApiRule::fieldSets(),
+            ],
+            'filter' => [
+                'nullable',
+                'array',
+                JsonApiRule::filter(),
+            ],
+            'include' => [
+                'nullable',
+                'string',
+                JsonApiRule::includePaths(),
+            ],
+            'page' => [
+                'nullable',
+                'array',
+                JsonApiRule::page(),
+            ],
+            'page.page' => ['integer', 'min:1'],
+            'page.limit' => ['integer', 'between:1,100'],
+            'sort' => [
+                'nullable',
+                'string',
+                JsonApiRule::sort(),
+            ],
+            'withCount' => [
+                'nullable',
+                'string',
+                JsonApiRule::countable(),
+            ],
+        ];
+    }
+}
