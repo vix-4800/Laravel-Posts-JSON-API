6850f34d4e5706c01868bbcdcaa5abfbc068a227
VIX <55950607+vix-4800@users.noreply.github.com>
2024-02-29 00:02:36 +0400

    tag resource added

---
+++ app/JsonApi/V1/Tags/TagResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\JsonApi\V1\Tags;
+
+use Illuminate\Http\Request;
+use LaravelJsonApi\Core\Resources\JsonApiResource;
+
+class TagResource extends JsonApiResource
+{
+    /**
+     * Get the resource's attributes.
+     *
+     * @param  Request|null  $request
+     */
+    public function attributes($request): iterable
+    {
+        return [
+            'name' => $this->name,
+        ];
+    }
+
+    /**
+     * Get the resource's relationships.
+     *
+     * @param  Request|null  $request
+     */
+    public function relationships($request): iterable
+    {
+        return [
+            $this->relation('posts'),
+        ];
+    }
+}
