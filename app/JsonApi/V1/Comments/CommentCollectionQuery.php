c2c555a0484d71abd271098af4823e2272eb6edf
VIX <55950607+vix-4800@users.noreply.github.com>
2024-02-29 01:40:48 +0400

    comment query validation added

---
+++ app/JsonApi/V1/Comments/CommentCollectionQuery.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\JsonApi\V1\Comments;
+
+use LaravelJsonApi\Laravel\Http\Requests\ResourceQuery;
+use LaravelJsonApi\Validation\Rule as JsonApiRule;
+
+class CommentCollectionQuery extends ResourceQuery
+{
+    /**
+     * Get the validation rules that apply to the request query parameters.
+     */
+    public function rules(): array
+    {
+        return [
+            'fields' => [
+                'nullable',
+                'array',
+                JsonApiRule::fieldSets(),
+            ],
+            'filter' => [
+                'nullable',
+                'array',
+                JsonApiRule::filter(),
+            ],
+            'include' => [
+                'nullable',
+                'string',
+                JsonApiRule::includePaths(),
+            ],
+            'page' => [
+                'nullable',
+                'array',
+                JsonApiRule::page(),
+            ],
+            'sort' => [
+                'nullable',
+                'string',
+                JsonApiRule::sort(),
+            ],
+            'withCount' => [
+                'nullable',
+                'string',
+                JsonApiRule::countable(),
+            ],
+        ];
+    }
+}
