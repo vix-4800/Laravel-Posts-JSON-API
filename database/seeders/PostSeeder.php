2bb93aa5797def7c75b0210ca7f19ddd73cbaeb4
vix-4800 <Gordan.ei@protonmail.ch>
2024-02-28 17:18:15 +0400

    post seeder changes - now some new posts will be created with categories and tags

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Post;
-use App\Models\User;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class PostSeeder extends Seeder
@@ -14,7 +15,19 @@ class PostSeeder extends Seeder
     public function run(): void
     {
         Post::factory(150)->create([
-            'author_id' => random_int(1, 15),
-        ]);
+            'author_id' => rand(1, 15),
+        ])->each(function (Post $post) {
+            if (rand(0, 1) === 1) {
+                $post->categories()->attach(
+                    Category::find(rand(1, 3))->pluck('id')->toArray()
+                );
+            }
+
+            if (rand(0, 1) === 1) {
+                $post->tags()->attach(
+                    Tag::find(rand(1, 3))->pluck('id')->toArray()
+                );
+            }
+        });
     }
 }
